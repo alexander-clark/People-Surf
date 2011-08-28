fb2e9f4f724a98af684e3cd854a16aec92c6fa96
Alexander Clark <sasha.jackal@gmail.com>
2011-08-28 16:17:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peoplesurf::Application.config.secret_token = 'c6d410b06cfcd3d1214d0d04d187f8d34af557555226cd53e045915be7f23c9079fc33d25866b9993c4caf200667021b9b1709b18034eae35f634c0a40ac54f2'
